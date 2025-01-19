dd0dc37f08192b7627df52e0cee8e3f04bbabbac
MICHELLE MARTINS <michelle.teixeira.124@ufrn.edu.br>
2025-01-19 18:42:13 -0300

    resolve endpoint post

--- src/main/java/com/br/alumind/controllers/OllamaController.java
+++ src/main/java/com/br/alumind/controllers/OllamaController.java
@@ -4,6 +4,7 @@ import com.br.alumind.models.FeedbackModel;
 import com.br.alumind.services.OllamaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
@@ -15,12 +16,12 @@ public class OllamaController {
     @Autowired
     private OllamaService chatService;
     public record Response(String justify, String Sentiment){}
-    @GetMapping("generate")
+    @PostMapping("generate")
     public Map generate(@RequestParam(value = "message") String message) {
         return Map.of("ollama", chatService.runClassify(message));
     }
 
-    @GetMapping("/generateRAG")
+    @PostMapping("/generateRAG")
     public Map generateRAG(@RequestParam(value = "message") String message) {
         try {
             return Map.of("ollama", chatService.runRAG(message));
