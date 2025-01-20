d446b44ec4793ecbfd64f588f5123718c114e7e0
MICHELLE MARTINS <michelle.teixeira.124@ufrn.edu.br>
2025-01-19 22:41:16 -0300

    update scripts python

--- python_scripts/scp.py
+++ python_scripts/scp.py
@@ -1,5 +1,31 @@
 import pandas as pd
 
+import pandas as pd
+
+def update_sentiment(input_file, output_file):
+    try:
+        # Carregar o dataset
+        df = pd.read_csv(input_file)
+
+        # Verificar se as colunas necessárias existem
+        if 'score' not in df.columns:
+            print("A coluna 'score' não foi encontrada no dataset.")
+            return
+        if 'sentiment' not in df.columns:
+            df['sentiment'] = ''  # Adicionar a coluna de sentimento, se não existir
+
+        # Atualizar a coluna 'sentiment' com base no valor de 'score'
+        df['sentiment'] = df['score'].apply(lambda x: 'negativo' if x < 3 else 'positivo' if x > 3 else 'Neutro')
+
+        # Salvar o dataset atualizado
+        df.to_csv(output_file, index=False)
+        print(f"Dataset atualizado salvo em: {output_file}")
+        return df
+
+    except Exception as e:
+        print(f"Erro ao processar o arquivo: {e}")
+
+
 def remove_column(file_path, column_name, output_path):
     try:
         # Carregar o dataset
@@ -18,10 +44,19 @@ def remove_column(file_path, column_name, output_path):
         print(f"Dataset atualizado salvo em: {output_path}")
     except Exception as e:
         print(f"Erro ao processar o arquivo: {e}")
+
+
+# Exemplo de uso
+input_path = "datasets/reviews_dataset.csv"  # Substitua pelo caminho do arquivo de entrada
+output_path = "datasets/dataset_atualizado.csv"  # Substitua pelo caminho do arquivo de saída
+
+# Chamar a função
+updated_df = update_sentiment(input_path, output_path)
  
 for column_name in ['reviewId', 'userImage','thumbsUpCount', 'reviewCreatedVersion', 'replyContent','repliedAt','sortOrder','appId']:
     
-    input_file = "dataset_atualizado.csv"  
+    input_file = output_path  
     column_to_remove = column_name        
-    output_file = "dataset_atualizado.csv"    
+    output_file = output_path    
     remove_column(input_file, column_to_remove, output_file)
+
